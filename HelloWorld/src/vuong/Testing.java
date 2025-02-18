3364a0efd2631d26069ddf5ff60459223a80adff
vuong1609 <vuong.h.64cntt@ntu.edu.vn>
2025-02-18 18:54:12 +0700

    HelloWorld
    Demo HelloWorld

---
+++ HelloWorld/src/vuong/Testing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package vuong;
+
+/**
+ * 
+ */
+public class Testing {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
